23fb2fbcfaa00e0016bec6e06d7480c32bf1b128
Miroslav Matejovsky <148510451+miroslav-matejovsky@users.noreply.github.com>
2025-08-07 10:40:09 +0200

    trying different folder structure (#2)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import markdoc from '@astrojs/markdoc';
-
 export default defineConfig({
   site: 'https://miroslav-matejovsky.github.io',
 
   vite: {
     plugins: [tailwindcss()],
-  },
+  }
 
-  integrations: [markdoc()],
 });
--- src/data/todo.md
+++ src/data/todo.md
@@ -1,7 +1,5 @@
 # TODO
 
-- make alias for powershell (psake)
 - packer and windows
 - replicating VMs for local dev
 - The Creative Act by Rick Rubin for devs
-- Windows sandbox cli
