6805fdb7814193e00dc20c1a79adb794ea8fa28f
Miroslav Matejovsky <miroslav.matejovsky@vissim.no>
2025-09-29 05:52:58 +0200

    more stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,22 +6,33 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Introverted Rebel',
+			title: 'Miroslav Matejovsky',
 			social: [
 				{ icon: 'github', label: 'GitHub', href: 'https://github.com/miroslav-matejovsky' },
 				{ icon: 'linkedin', label: 'LinkedIn', href: 'https://www.linkedin.com/in/miroslav-matejovsky/' }
 			],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Elixir',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						// { label: 'First taste', slug: 'elixir/first-taste' },
+					],
+				},
+				{ label: 'Go',
+					items: [
+						// { label: 'Installers with Embedding', slug: 'go/installers-with-embedding' },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'About',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'About Me', slug: 'about/me' },
+						// { label: 'Stoic', slug: 'about/about-stoic' },
+						// { label: 'This Page', slug: 'about/this-page' },
+						// { label: 'CV', slug: 'cv' },
+					],
+					// autogenerate: { directory: 'reference' },
 				},
 			],
 		}),
---
+++ src/content/docs/about/me.md
@@ -0,0 +1,8 @@
+---
+title: About Me
+description: Learn more about Miroslav Matejovsky.
+tableOfContents: false
+---
+
+Hello! I'm Miroslav Matejovsky, an introverted rebel, latent artist, and practicing stoic.
+Welcome to my personal website where I share my thoughts, notes, and articles about software programming, software architecture, and more.
